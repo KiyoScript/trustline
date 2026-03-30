8e815242762288d9318ada943da9b844081a55e5
kiyoscript <johnlloyddesape@gmail.com>
2026-03-30 14:09:33 +0800

    Add role column to users and define user roles as an enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum :role, {
+    super_admin: 0,
+    admin:       1,
+    manager:     2,
+    agent:       3,
+    viewer:      4
+  }
 end
